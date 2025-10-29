dcd495f148262ca2e76cdde08f8394f8865b372c
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-29 17:00:22 -0600

    Agregao entrypoint a MainActivity.kt

--- app/src/main/java/edu/ucne/nathiel_taveras_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/nathiel_taveras_ap2_p2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.nathiel_taveras_ap2_p2.ui.theme.Nathiel_Taveras_AP2_P2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
